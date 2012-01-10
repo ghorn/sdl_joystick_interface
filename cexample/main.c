0c6e17fb6247696f8bd6642e74198cb59a7b7416
Greg Horn <gregmainland@gmail.com>
2012-01-09 21:42:50 -0800

    removed unnessecary run_realtime

--- cexample/main.c
+++ cexample/main.c
@@ -2,9 +2,10 @@
 // (c) 2011, Greg Horn
 
 #include <stdio.h>
+#include <unistd.h>
+
 
 #include "../csrc/sdl_joystick_interface.h"
-#include "run_realtime.h"
 
 int
 main()
@@ -17,7 +18,7 @@ main()
     printf("throttle: % .3f, yaw: % .3f, pitch: % .3f, roll: % .3f, "
            "enable: %d, mode: %d, gear: %d, aux2: %d\n",
            rc->throttle,rc->yaw,rc->pitch,rc->roll,rc->enable,rc->mode,rc->gear,rc->aux2);
-    run_realtime(0.1);
+    usleep(100000);
   }
 
 }
